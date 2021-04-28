d453fb627acb407de631fb44cad7ffc6964483e8
Visal So <sovisal.sv@gmail.com>
2021-04-28 11:44:29 -0500

    Made work with our local project folder outside of Chengyuan's onedrive

--- cam.bat
+++ cam.bat
@@ -4,4 +4,4 @@ call %root%\Scripts\activate.bat %root%
 
 call conda activate cam37
 
-call python "C:\Users\TrappedIonPC\cq5\OneDrive - rice.edu\Devices\BFS-PGE-120S4M\myapp\start.py"
+call python "C:\Software Programming RiceYb\FlirCamera\start.py"
