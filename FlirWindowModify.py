2f5214068379233ac28665fe8efbad12b2d8c88d
April Sheffield <axs27@rice.edu>
2022-09-06 15:34:09 -0500

    Added capability to fit multiple gaussians at a time, logging of beam centers over time, and flipped the y axis on the y cross-section graph so it matches the image

--- FlirWindowModify.py
+++ FlirWindowModify.py
@@ -1,3 +1,6 @@
+import datetime
+import time
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPainter, QPen
 from PyQt5.QtCore import QRect, QPoint
@@ -5,7 +8,7 @@ from PyQt5.QtWidgets import QScrollBar, QScrollArea, QWidget, QApplication
 from pyqtgraph import PlotWidget
 from pyqtgraph import mkPen
 from FlirWindow import Ui_MainWindow
-from fitgauss import fitgauss2d_section, gauss1d
+from fitgauss import fitgauss2d_section, gauss1d, fitgauss2d_multiple
 import numpy as np
 import os
 import cv2
@@ -38,9 +41,21 @@ class Ui_CustomWindow(Ui_MainWindow):
         self.zoom_scale_x = 0.0
         self.zoom_scale_y = 0.0
 
+        # multiple fits line edit
+        self.num_fits = 1
+        self.lineEditMultiFits.setText('1')
+        self.line_edit_multi_fits()
+        # self.lineEditMultiFits.stateChanged.connect(self.line_edit_multi_fits)
+
+        self.slices = 30
+        self.lineEditSlices.setText('30')
+        self.line_edit_slices()
+
         # Data for the fitted gaussian
         self.section_xdata_fit = []
+        self.section_xdata_fit_mult = [[] for _ in range(self.num_fits)]
         self.section_ydata_fit = []
+        self.section_ydata_fit_mult = [[] for _ in range(self.num_fits)]
 
 
         #logo
@@ -66,8 +81,14 @@ class Ui_CustomWindow(Ui_MainWindow):
         self.plotx.setObjectName("plotx")
         self.gridLayoutImage.addWidget(self.plotx, 1, 0, 1, 1)
 
+        self.colors = [(np.random.randint(0, 256), np.random.randint(0, 256), np.random.randint(0, 256))
+                       for _ in range(self.num_fits)]
+
         self.sectionx_fit = self.plotx.plot(self.section_xcoord, self.section_xdata_fit,
                                             pen=mkPen(color=(0, 255, 0), style=QtCore.Qt.DashLine))
+        self.sectionx_fit_mult = [self.plotx.plot(self.section_xcoord, self.section_xdata_fit_mult[i],
+                                                  pen=mkPen(color=self.colors[i], style=QtCore.Qt.DashLine))
+                                  for i in range(len(self.section_xdata_fit_mult))]
 
         self.sectionx_line = self.plotx.plot(self.section_xcoord,self.section_xdata)
 
@@ -75,11 +96,32 @@ class Ui_CustomWindow(Ui_MainWindow):
 
         self.ploty.setObjectName("ploty")
         self.gridLayoutImage.addWidget(self.ploty,  0, 1, 1, 1)
+        self.ploty.getPlotItem().getViewBox().invertY(True)
 
         self.sectiony_fit = self.ploty.plot(self.section_ydata_fit, self.section_ycoord,
                                             pen=mkPen(color=(0, 255, 0), style=QtCore.Qt.DashLine))
+        self.sectiony_fit_mult = [self.ploty.plot(self.section_ydata_fit_mult[i], self.section_ycoord,
+                                                  pen=mkPen(color=self.colors[i], style=QtCore.Qt.DashLine))
+                                  for i in range(len(self.section_ydata_fit_mult))]
 
         self.sectiony_line = self.ploty.plot(self.section_ydata,self.section_ycoord)
+
+        # Display fit parameters for multiple fits in colors that match the plots
+        self.param_displays = [[] for _ in range(5)]
+        self.param_labels = ('xCenter', 'yCenter', 'xWaist', 'yWaist', 'Height')
+        self.param_layout = QtWidgets.QGridLayout()
+        self.param_layout.setObjectName('gridLayoutParameters')
+        self.param_label_layout = QtWidgets.QGridLayout()
+        self.param_label_layout.setObjectName('gridLayoutParameterLabels')
+        # if True:
+        #     self.param_displays = [[QtWidgets.QLabel(self.centralwidget) for _ in range(self.num_fits)] for _ in range(5)]
+        #     for i in range(len(self.param_displays)):
+        #         for j in range(len(self.param_displays[i])):
+        #             self.param_displays[i][j].setFont(self.param_font)
+        #             self.param_displays[i][j].setObjectName("display" + self.param_labels[i] + str(j + 1))
+        #             self.param_displays[i][j].setStyleSheet('color: rgb(' + str(self.colors[j][0]) + ', '
+        #                                                     + str(self.colors[j][1]) + ', ' + str(self.colors[j][2]) + ');')
+        #             self.gridLayoutFitResult.addWidget(self.param_displays[i][j], i, j + 1, 1, 1)
 
         # set section to the center
         self.pushButtonSectionCenter.clicked.connect(self.section_center)
@@ -119,6 +161,12 @@ class Ui_CustomWindow(Ui_MainWindow):
         self.temperature_timer.timeout.connect(self.update_temperature)
         self.temperature_timer.start(1000)
 
+        # center logging
+        self.start_date = str(datetime.datetime.now()).replace(':', '-')
+        self.start_time = time.time()
+        with open('center_log/' + self.start_date + '.txt', 'w') as f:
+            f.write('time, x0, y0, x1, y1...\n')
+
     def start_continue(self):
         self.cam_controller.start_continue()
         self.pushButtonContinue.setText("Stop Continue")
@@ -140,15 +188,75 @@ class Ui_CustomWindow(Ui_MainWindow):
     def update_movie(self):
         self.cam_controller.acquire_continue()
         if self.checkBoxFit.isChecked():
-            self.p, self.ier = fitgauss2d_section(np.arange(0, self.cam_controller.frame.shape[1]),
-                                        np.arange(0, self.cam_controller.frame.shape[0]), self.cam_controller.frame)
-            self.update_plot()
-            # Display measurements in pixels instead of microns (if we want to convert back to microns, multiply by self.unit)
-            self.lineEditxCenter.setText('%.1f' % (self.p[0]))
-            self.lineEdityCenter.setText('%.1f' % (self.p[1]))
-            self.lineEditxWaist.setText('%.1f' % (self.p[2] * 2*self.unit))
-            self.lineEdityWaist.setText('%.1f' % (self.p[3] * 2*self.unit))
-            self.lineEditHeight.setText('%.1f' % (self.p[4]))
+            self.line_edit_multi_fits()
+            self.line_edit_slices()
+            if self.num_fits <= 1:
+                self.p, self.ier = fitgauss2d_section(np.arange(0, self.cam_controller.frame.shape[1]),
+                                            np.arange(0, self.cam_controller.frame.shape[0]), self.cam_controller.frame)
+                self.update_plot()
+                # Display measurements in pixels instead of microns (if we want to convert back to microns, multiply by self.unit)
+                self.lineEditxCenter.setText('%.1f' % (self.p[0]))
+                self.lineEdityCenter.setText('%.1f' % (self.p[1]))
+                self.lineEditxWaist.setText('%.1f' % (self.p[2] * 2*self.unit))
+                self.lineEdityWaist.setText('%.1f' % (self.p[3] * 2*self.unit))
+                self.lineEditHeight.setText('%.1f' % (self.p[4]))
+            else:
+                self.p = fitgauss2d_multiple(self.cam_controller.frame, np.arange(self.cam_controller.frame.shape[0]),
+                                             np.arange(self.cam_controller.frame.shape[1]), num_fits=self.num_fits,
+                                             slices=self.slices)
+                self.update_plot()
+                # self.p =
+                # self.lineEditxCenter.setText(str(['%.1f' % (pars[0]) for pars in self.p]))
+                # self.lineEdityCenter.setText(str(['%.1f' % (pars[1]) for pars in self.p]))
+                # self.lineEditxWaist.setText(str(['%.1f' % (pars[2] * 2*self.unit) for pars in self.p]))
+                # self.lineEdityWaist.setText(str(['%.1f' % (pars[3] * 2*self.unit) for pars in self.p]))
+                # self.lineEditHeight.setText(str(['%.1f' % (pars[4]) for pars in self.p]))
+
+                self.gridLayoutFitResult.addLayout(self.param_label_layout, 0, 0, 1, 1)
+                self.gridLayoutFitResult.addLayout(self.param_layout, 0, 1, 1, 1)
+                self.lineEditxCenter.setParent(None)
+                self.lineEdityCenter.setParent(None)
+                self.lineEditxWaist.setParent(None)
+                self.lineEdityWaist.setParent(None)
+                self.lineEditHeight.setParent(None)
+
+                self.labelxCenter.setParent(None)
+                self.param_label_layout.addWidget(self.labelxCenter, 0, 0, 1, 1)
+                self.labelyCenter.setParent(None)
+                self.param_label_layout.addWidget(self.labelyCenter, 1, 0, 1, 1)
+                self.labelxWaist.setParent(None)
+                self.param_label_layout.addWidget(self.labelxWaist, 2, 0, 1, 1)
+                self.labelyWaist.setParent(None)
+                self.param_label_layout.addWidget(self.labelyWaist, 3, 0, 1, 1)
+                self.labelHeight.setParent(None)
+                self.param_label_layout.addWidget(self.labelHeight, 4, 0, 1, 1)
+
+                while self.num_fits > len(self.param_displays[0]):
+                    for i in range(5):
+                        self.param_displays[i].append(QtWidgets.QLabel(self.centralwidget))
+                        self.param_displays[i][-1].setFont(self.param_font)
+                        self.param_displays[i][-1].setObjectName("display" + self.param_labels[i] +
+                                                                 str(len(self.param_displays[i])))
+                        self.param_displays[i][-1].setStyleSheet('color: rgb(' +
+                                                                 str(self.colors[len(self.param_displays[i]) - 1][0])
+                                                                 + ', ' +
+                                                                 str(self.colors[len(self.param_displays[i]) - 1][1])
+                                                                 + ', ' +
+                                                                 str(self.colors[len(self.param_displays[i]) - 1][2])
+                                                                 + ');')
+                        self.param_layout.addWidget(self.param_displays[i][-1], i, len(self.param_displays[i]),
+                                                           1, 1)
+                while self.num_fits < len(self.param_displays[0]):
+                    for i in range(5):
+                        self.param_displays[i][-1].setText('')
+                        self.param_displays[i].pop(-1)
+                for i in range(5):
+                    for j in range(len(self.param_displays[i])):
+                        if 2 <= i <= 3:
+                            self.param_displays[i][j].setText('%.1f' % (self.p[j][i] * 2 * self.unit))
+                        else:
+                            self.param_displays[i][j].setText('%.1f' % self.p[j][i])
+
         else:
             self.update_plot_withoutfit()
             self.lineEditxCenter.setText('N/A')
@@ -230,10 +338,19 @@ class Ui_CustomWindow(Ui_MainWindow):
 
         self.section_xdata_fit = []
         self.sectionx_fit.setData(self.section_xcoord, self.section_xdata_fit)
+        self.section_xdata_fit_mult = [[] for _ in range(self.num_fits)]
+        for i in range(len(self.sectionx_fit_mult)):
+            self.sectionx_fit_mult[i].setData(self.section_xcoord, [])
         self.section_ydata_fit = []
         self.sectiony_fit.setData(self.section_ydata_fit, self.section_ycoord)
+        self.section_ydata_fit_mult = [[] for _ in range(self.num_fits)]
+        for i in range(len(self.sectiony_fit_mult)):
+            self.sectiony_fit_mult[i].setData(self.section_ydata_fit_mult[i], self.section_ycoord)
 
     def update_plot(self):
+        self.line_edit_multi_fits()
+        self.line_edit_slices()
+
         self.section_xcoord = np.arange(0, self.cam_controller.frame.shape[1])
         self.section_xdata = self.cam_controller.frame[self.section_yctr,::]
         self.sectionx_line.setData(self.section_xcoord, self.section_xdata)
@@ -241,12 +358,74 @@ class Ui_CustomWindow(Ui_MainWindow):
         self.section_ydata = self.cam_controller.frame[::,self.section_xctr]
         self.sectiony_line.setData(self.section_ydata,self.section_ycoord)
 
-        # Create the lines for the fitted graph to be plotted below the data
-        self.section_xdata_fit = self.gauss_data(self.p[4], self.p[0], self.p[2], self.p[5], self.section_xcoord.shape[0])
-        self.sectionx_fit.setData(self.section_xcoord, self.section_xdata_fit)
+        # print('test2')
 
-        self.section_ydata_fit = self.gauss_data(self.p[4], self.p[1], self.p[3], self.p[5], self.section_ycoord.shape[0])
-        self.sectiony_fit.setData(self.section_ydata_fit, self.section_ycoord)
+        if self.num_fits <= 1 or not isinstance(self.p[0], list):
+            # Create the lines for the fitted graph to be plotted below the data
+            self.section_xdata_fit = self.gauss_data(self.p[4], self.p[0], self.p[2], self.p[5], self.section_xcoord.shape[0])
+            self.sectionx_fit.setData(self.section_xcoord, self.section_xdata_fit)
+            self.section_xdata_fit_mult = [[] for _ in range(self.num_fits)]
+            for i in range(len(self.sectionx_fit_mult)):
+                self.sectionx_fit_mult[i].setData(self.section_xcoord, [0 for _ in self.section_xcoord])
+
+            self.section_ydata_fit = self.gauss_data(self.p[4], self.p[1], self.p[3], self.p[5], self.section_ycoord.shape[0])
+            self.sectiony_fit.setData(self.section_ydata_fit, self.section_ycoord)
+            self.section_ydata_fit_mult = [[] for _ in range(self.num_fits)]
+            for i in range(len(self.sectiony_fit_mult)):
+                self.sectiony_fit_mult[i].setData([0 for _ in self.section_ycoord], self.section_ycoord)
+        else:
+            self.section_xdata_fit = []
+            self.sectionx_fit.setData(self.section_xcoord, [0 for _ in self.section_xcoord])
+            self.section_ydata_fit = []
+            self.sectiony_fit.setData([0 for _ in self.section_ycoord], self.section_ycoord)
+            self.section_xdata_fit_mult = [[] for _ in range(len(self.p))]
+            for i in range(len(self.sectionx_fit_mult)):
+                self.sectionx_fit_mult[i].setData(self.section_xcoord, [0 for _ in self.section_xcoord])
+            self.section_ydata_fit_mult = [[] for _ in range(len(self.p))]
+            for i in range(len(self.sectiony_fit_mult)):
+                self.sectiony_fit_mult[i].setData([0 for _ in self.section_ycoord], self.section_ycoord)
+            for fit_num in range(len(self.p)):
+                # print('test3')
+                # self.section_xdata_fit = (self.gauss_data(fit[4], fit[0], fit[2], fit[5], self.section_xcoord.shape[0]))
+                # self.sectionx_fit.setData(self.section_xcoord, self.section_xdata_fit)
+                # self.plotx.plot(self.section_xcoord, self.section_xdata_fit,
+                #                 pen=mkPen(color=(0, 255, 0), style=QtCore.Qt.DashLine))
+                self.section_xdata_fit_mult[fit_num] = self.gauss_data(self.p[fit_num][4], self.p[fit_num][0],
+                                                                       self.p[fit_num][2], self.p[fit_num][5],
+                                                                       self.section_xcoord.shape[0])
+                if len(self.colors) <= fit_num:
+                    self.colors.append((np.random.randint(0, 256), np.random.randint(0, 256), np.random.randint(0, 256)))
+                if len(self.sectionx_fit_mult) <= fit_num:
+                    self.sectionx_fit_mult.append(self.plotx.plot(self.section_xcoord,
+                                                                  self.section_xdata_fit_mult[fit_num],
+                                                  pen=mkPen(color=self.colors[-1], style=QtCore.Qt.DashLine)))
+                else:
+                    self.sectionx_fit_mult[fit_num].setData(self.section_xcoord, self.section_xdata_fit_mult[fit_num])
+
+                # self.section_ydata_fit = (self.gauss_data(fit[4], fit[1], fit[3], fit[5], self.section_ycoord.shape[0]))
+                # self.sectiony_fit.setData(self.section_ydata_fit, self.section_ycoord)
+                # self.ploty.plot(self.section_ydata_fit, self.section_ycoord,
+                #                 pen=mkPen(color=(0, 255, 0), style=QtCore.Qt.DashLine))
+                self.section_ydata_fit_mult[fit_num] = self.gauss_data(self.p[fit_num][4], self.p[fit_num][1],
+                                                                       self.p[fit_num][3], self.p[fit_num][5],
+                                                                       self.section_ycoord.shape[0])
+                if len(self.sectiony_fit_mult) <= fit_num:
+                    self.sectiony_fit_mult.append(self.ploty.plot(self.section_ydata_fit_mult[fit_num],
+                                                                  self.section_ycoord,
+                                                                  pen=mkPen(color=self.colors[-1],
+                                                                            style=QtCore.Qt.DashLine)))
+                else:
+                    self.sectiony_fit_mult[fit_num].setData(self.section_ydata_fit_mult[fit_num], self.section_ycoord)
+
+        # center logging
+        with open('center_log/' + self.start_date + '.txt', 'a') as f:
+            if self.num_fits <= 1 or not isinstance(self.p[0], list):
+                f.write(str(time.time() - self.start_time) + ', ' + str(self.p[0]) + ', ' + str(self.p[1]) + '\n')
+            else:
+                f.write(str(time.time() - self.start_time))
+                for fit in self.p:
+                    f.write(', ' + str(fit[0]) + ', ' + str(fit[1]))
+                f.write('\n')
 
     # Gaussian function
     def gauss(self, t, A, mu, sigma, offset):
@@ -378,8 +557,12 @@ class Ui_CustomWindow(Ui_MainWindow):
         # self.section_yctr = round(float(self.lineEdityCenter.text())/self.unit)
 
         # Instead change center to the centers of each gaussian
-        self.section_xctr = round(self.p[0])
-        self.section_yctr = round(self.p[1])
+        if self.num_fits <= 1:
+            self.section_xctr = round(self.p[0])
+            self.section_yctr = round(self.p[1])
+        else:
+            self.section_xctr = round(self.p[0][0])
+            self.section_yctr = round(self.p[0][1])
 
         self.lineEditSectionX.setText(str(self.section_xctr))
         self.lineEditSectionY.setText(str(self.section_yctr))
@@ -398,6 +581,18 @@ class Ui_CustomWindow(Ui_MainWindow):
             self.cam_controller.reset_exposure()
         else:
             self.set_exptime()
+
+    def line_edit_multi_fits(self):
+        if int(self.lineEditMultiFits.text()) > 0:
+            self.num_fits = int(self.lineEditMultiFits.text())
+        else:
+            self.num_fits = 1
+
+    def line_edit_slices(self):
+        if int(self.lineEditSlices.text()) > 0:
+            self.slices = int(self.lineEditSlices.text())
+        else:
+            self.slices = 30
 
     def set_background(self):
         self.cam_controller.set_background()
