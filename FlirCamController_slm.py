2e44a4fbb46a4f24e7c9700913333e7224901243
Abhishek Menon <am219@rice.edu>
2024-12-02 17:09:04 -0600

    For SLM use

--- FlirCamController_slm.py
+++ FlirCamController_slm.py
@@ -7,6 +7,7 @@ from numpy import zeros, uint8
 import time
 import cv2
 import os
+import datetime
 class FlirCamController:
     def initialize(self):
         self.count=1
@@ -382,22 +383,15 @@ class FlirCamController:
 
         return True
 
-    def file_save(self,item):
+    def file_save(self,path):
         frametosave = self.frame
 
 
-        if item==0:
-            directory=r'C:\Users\RiceT\Documents\FlirCamera\pictures'
-        if item==1:
-            directory=r'C:\Users\RiceT\Documents\FlirCamera\pictures\image1'
-        if item==2:
-            directory=r'C:\Users\RiceT\Documents\FlirCamera\pictures\image2'
-        if item==3:
-            directory=r'C:\Users\RiceT\Documents\FlirCamera\pictures\image3'
-        self.count = str(time.time())
-        name = f"image_{self.count}.png"
-        self.file_path=os.path.join(directory,name)
-        cv2.imwrite(self.file_path, frametosave)
+        try:
+            cv2.imwrite(path, frametosave)
+        except Exception as e:
+            print('save error:',e)
+
 
     def setSize(self, xoffset, yoffset, width, height):
         try:
@@ -475,6 +469,13 @@ class FlirCamController:
             else:
                 print("Height not available...")
                 result = False
+            self.framewidth = self.cam.Width.GetValue()
+            self.frameheight = self.cam.Height.GetValue()
+            self.pixel_size *= 1
+            self.frame = zeros((self.frameheight, self.framewidth), dtype=uint8)
+            self.background = zeros((self.frameheight, self.framewidth), dtype=uint8)
+            self.nobackground = zeros((self.frameheight, self.framewidth), dtype=uint8)
+            self.floatzeroframe = zeros((self.frameheight, self.framewidth))
         except PySpin.SpinnakerException as ex:
             print("Error: %s" % ex)
             return False
--- save_image.py
+++ save_image.py
@@ -1,16 +1,12 @@
 import select
-import  sys
-print(sys.prefix)
 from FlirCamController_slm import FlirCamController
-
-from FlirCamFake import FakeCamController
-import qdarkstyle
-
+import  os
+import  datetime
 import socket
 import struct
 import time
 import math
-from PyQt5 import QtWidgets
+
 def save_image():
     print('qidong')
     flir = FlirCamController()
@@ -18,31 +14,74 @@ def save_image():
     flir.start_continue()
     flir.set_average_frames(1)
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('localhost', 49958))#49957
+    client_socket.connect(('localhost', 51814))#49957
 
     inputs=[client_socket]
     timeout = 0.01
     while True:
 
-        try:
+        if 1:
             readable,writable,exceptional = select.select(inputs,[],[],timeout)
             if readable:
                 data = client_socket.recv(1024)
                 data=data.decode('utf-8')
 
                 if 'run' in data:
+                    if len(data)>4:
+                        now = (datetime.datetime.now().strftime("d%d_h%H_m%M_s%S"))
+                        directory = r'C:\Users\RiceT\Documents\FlirCamera\pictures\image_intensity'
+                        name = f"image_{now}.bmp"
+                        path = os.path.join(directory, name)
+                    else:
+                        now = (datetime.datetime.now().strftime("d%d_h%H_m%M_s%S"))
+                        directory = r'C:\Users\RiceT\Documents\FlirCamera\pictures\image0'
+                        name = f"image_{now}.bmp"
+                        path = os.path.join(directory, name)
+
                     flir.acquire_continue()
-                    flir.file_save(0)
-                    message= flir.file_path.encode('utf-8')
+                   
+                    
+                    flir.file_save(path)
+                    message= path.encode('utf-8')
                     client_socket.sendto(message,('localhost', 49958))
                 if 'take' in data:
                     flir.acquire_continue()
-                    data_=int(data[4:])
+                    data_=data[4:]
+                    if data_ == '1':
+                        directory = r'C:\Users\RiceT\Documents\FlirCamera\pictures\image1'
+                        now = (datetime.datetime.now().strftime("d%d_h%H_m%M_s%S"))
+                    if data_ == '2':
+                        directory = r'C:\Users\RiceT\Documents\FlirCamera\pictures\image2'
+                        now = (datetime.datetime.now().strftime("d%d_h%H_m%M_s%S"))
+                    if data_ == '3':
+                        directory = r'C:\Users\RiceT\Documents\FlirCamera\pictures\image3'
+                        now = (datetime.datetime.now().strftime("d%d_h%H_m%M_s%S"))
+                    if len(data_)>1:
+                        directory = r'C:\Users\RiceT\Documents\FlirCamera\pictures\correction1'
+                        now = data_
+
+                    name = f"image_{now}.bmp"
+                    path = os.path.join(directory, name)
                     message = b'acquire finish'
                     client_socket.sendto(message, ('localhost', 49958))
-                    flir.file_save(data_)
+                    flir.file_save(path)
                     message = b'save finish'
                     client_socket.sendto(message, ('localhost', 49958))
+                if 'took' in data:
+                    flir.acquire_continue()
+                    data_=data[4:]
+
+                    if len(data_)>1:
+                        directory = r'C:\Users\RiceT\Documents\FlirCamera\pictures\correction2'
+                        now = data_
+
+                    name = f"image_{now}.bmp"
+                    path = os.path.join(directory, name)
+                    message = b'acquire finish'
+                    client_socket.sendto(message, ('localhost',51814))
+                    flir.file_save(path)
+                    message = b'save finish'
+                    client_socket.sendto(message, ('localhost', 51814))
                 if 'auto_expo' in data:
                     flir.reset_exposure()
                     print(flir.get_exposure())
@@ -85,10 +124,15 @@ def save_image():
                     print(offsetx, offsety, width, Height)
                     flir.setSize(offsetx, offsety, width, Height)
                     flir.start_continue()
+                if 'disconnect' in data:
+                    flir.stop_continue()
+                    flir.close()
+                    client_socket.close()
+                    return 0
             time.sleep(0.01)
-        except Exception as e :
-            print('error_image_save:',e)
-            time.sleep(0.01)
+        # except Exception as e :
+        #     print('error_image_save:',e)
+        #     time.sleep(0.01)
 
     flir.stop_continue()
     flir.close()
